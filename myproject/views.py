d918d47fec2fb150c5e9add60e33feb762186699
“Leo <lcockerill@student.saintaug.nsw.edu.au>
2024-05-15 13:41:01 +1000

    Finish Templates

--- myproject/views.py
+++ myproject/views.py
@@ -2,10 +2,16 @@
 #https://flask.palletsprojects.com/en/3.0.x/tutorial/views/#create-a-blueprint
 
 
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+
+
+
+
+
 
 views = Blueprint('views', __name__)
 
 @views.route('/') #this is the route
 def home(): #this is the function that will be executed when the route is accessed
-    return "<h1>Hello World</h1>"
+    return render_template("home.html") #this is the template that will be rendered when the route is accessed
