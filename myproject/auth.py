5866a4e0fffa188113a352c7127d280e621df8f9
“Leo <lcockerill@student.saintaug.nsw.edu.au>
2024-05-21 14:27:25 +1000

    Set up the base to flash a message. Verify if user details are valid.

--- myproject/auth.py
+++ myproject/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
@@ -20,18 +20,18 @@ def sign_up():
 
 
         if len(email) <4:
-            pass
+            flash('Email must be greater than 4 characters.', category='error')
 
         elif len(firstName) <2:
-            pass
+            flash('First name must be greater than 2 characters.', category='error')
 
         elif password1 != password2:
-            pass
+            flash('Passwords don\'t match.', category='error')
 
         elif len(password1) <7:
-            pass
+            flash('Password must be at least 7 characters.', category='error')
         else:
-            #add user to database
+            flash('Account Created!', category='success') # add user to database
             pass
         
 
