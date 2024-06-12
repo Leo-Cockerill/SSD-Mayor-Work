9aae8a1e883c0d4b69ef06a6656dfa4966519f59
“Leo <lcockerill@student.saintaug.nsw.edu.au>
2024-06-12 12:51:37 +1000

    Working on Recipe page

--- main.py
+++
@@ -1,7 +0,0 @@
-from myproject import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True) #This will run the app in debug mode. Debug mode allows us to see the changes we make to the app in real time.
-
